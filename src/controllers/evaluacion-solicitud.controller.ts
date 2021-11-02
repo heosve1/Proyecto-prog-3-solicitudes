a7e90a7ede3fd24b3328f1fa629a1205fafa1730
Heosve <juanjosehenao1810@gmail.com>
2021-11-02 03:10:41 -0500

    commit

--- src/controllers/evaluacion-solicitud.controller.ts
+++ src/controllers/evaluacion-solicitud.controller.ts
@@ -1,34 +1,30 @@
-import {service} from '@loopback/core';
 import {
   Count,
   CountSchema,
   Filter,
   FilterExcludingWhere,
   repository,
-  Where
+  Where,
 } from '@loopback/repository';
 import {
-  del, get,
-  getModelSchemaRef, param, patch, post, put, requestBody,
-  response
+  post,
+  param,
+  get,
+  getModelSchemaRef,
+  patch,
+  put,
+  del,
+  requestBody,
+  response,
 } from '@loopback/rest';
-import {Evaluacionsolicitud, NotificacionCorreo} from '../models';
-import {EvaluacionsolicitudRepository, JuradoRepository, SolicitudRepository} from '../repositories';
-import {NotificacionesService} from '../services';
-
+import {Evaluacionsolicitud} from '../models';
+import {EvaluacionsolicitudRepository} from '../repositories';
 
 export class EvaluacionSolicitudController {
   constructor(
     @repository(EvaluacionsolicitudRepository)
-    public evaluacionsolicitudRepository: EvaluacionsolicitudRepository,
-    @repository(JuradoRepository)
-    public juradoRepository: JuradoRepository,
-    @service(NotificacionesService)
-    public servicioNotificaciones: NotificacionesService,
-    @repository(SolicitudRepository)
-    public solicitudRepository: SolicitudRepository,
-
-  ) { }
+    public evaluacionsolicitudRepository : EvaluacionsolicitudRepository,
+  ) {}
 
   @post('/evaluacionsolicitudes')
   @response(200, {
@@ -48,16 +44,6 @@ export class EvaluacionSolicitudController {
     })
     evaluacionsolicitud: Omit<Evaluacionsolicitud, 'id'>,
   ): Promise<Evaluacionsolicitud> {
-    let jurado = await this.juradoRepository.findById(evaluacionsolicitud.id_jurado);
-    let solicitud = await this.solicitudRepository.findById(evaluacionsolicitud.id_solicitud);
-    if (jurado && solicitud) {
-      let notificacion = new NotificacionCorreo();
-      notificacion.destinatario = jurado.correo;
-      notificacion.asunto = "Invitación Solicitud";
-      notificacion.mensaje = `<strong><h1 style = "font-size:150%;">Hola ${jurado.nombre}</h1></strong><br /> Se te ha hecho la solicitud de evaluar el trabajo <strong>${solicitud.nombre_trabajo}</strong>. <br /> Para aceptar evaluar este trabajo, ingresa al siguiente link: <a href="https://www.ucaldas.edu.co/portal/">Aceptar</a><br />Si deseas rechazar la evaluación, ingresa al siguiente link: <a href="https://tenor.com/view/shitpost-gun-gif-21896048">Rechazar</a> <br />Fecha:${solicitud.fecha}`
-      this.servicioNotificaciones.EnviarCorreo(notificacion)
-      console.log("Se ha notificado al usuario con exito")
-    }
     return this.evaluacionsolicitudRepository.create(evaluacionsolicitud);
   }
 
@@ -86,7 +72,6 @@ export class EvaluacionSolicitudController {
   })
   async find(
     @param.filter(Evaluacionsolicitud) filter?: Filter<Evaluacionsolicitud>,
-
   ): Promise<Evaluacionsolicitud[]> {
     return this.evaluacionsolicitudRepository.find(filter);
   }
@@ -106,7 +91,6 @@ export class EvaluacionSolicitudController {
     })
     evaluacionsolicitud: Evaluacionsolicitud,
     @param.where(Evaluacionsolicitud) where?: Where<Evaluacionsolicitud>,
-
   ): Promise<Count> {
     return this.evaluacionsolicitudRepository.updateAll(evaluacionsolicitud, where);
   }
@@ -122,7 +106,6 @@ export class EvaluacionSolicitudController {
   })
   async findById(
     @param.path.number('id') id: number,
-
     @param.filter(Evaluacionsolicitud, {exclude: 'where'}) filter?: FilterExcludingWhere<Evaluacionsolicitud>
   ): Promise<Evaluacionsolicitud> {
     return this.evaluacionsolicitudRepository.findById(id, filter);
